6a0c3d598037f568c56a404f083453d891f54225
Yaro <nikozhts@gmail.com>
2018-02-27 12:50:17 +0200

    add connect to mlab DB

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var bodyParser = require('body-parser')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl = 'mongodb://uname:pwname@ds135196.mlab.com:35196/l-node'
 
 var messages = [
     {name: 'Mike', message: 'Hi'},
@@ -26,6 +29,10 @@ app.post('/messages', (req, res) => {
 io.on('connection', (socket) => {
     console.log('a user connected')
 })
+
+mongoose.connect(dbUrl, {useMongoClient: true}, (err) => {
+    console.log('mongo db connection', err)
+} )
 
 var server = http.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
