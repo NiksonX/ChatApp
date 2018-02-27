cd3ac641e9371f84b6a9ef6086e7d624db29c70e
Yaro <nikozhts@gmail.com>
2018-02-27 16:56:12 +0200

    add test

--- spec/server.spec.js
+++ spec/server.spec.js
@@ -1,5 +1,19 @@
+import { request } from 'https';
+
+var require = require('request')
+
 describe('calc', () => {
     it('should multiply 3 and 3', () =>{
         expect(3*3).toBe(9)
+    })
+})
+
+describe('get messages', () =>{
+    it('should return 200 Ok', (done) => {
+        request.get('http://localhost:3000/messages', (err, res) => {
+            console.log(res.body)
+            done()
+        })
+
     })
 })
